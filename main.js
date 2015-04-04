50e461f0b3435462b410cbb72e9bc545a55544c3
mouseroot <mouseroot@mousetech.org>
2015-04-04 12:39:33 -0500

    created template engine

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 var express = require("express");
+var fs = require("fs");
 var path = require("path");
 var formidable = require("formidable");
 var app = express();
 
-var index_page = "<!DOCTYPE html><html lang='en-US'><head><meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'></head><body>" + 
-				"<form action='/upload' method='post' enctype='multipart/form-data'>" +
-				"<input type='file' name='upload-file' style='width=100%;height=250px'>" +
-				"<input type='submit' value='Upload'>" +
-				"</form></body></html>";
+/* 
+	Simple Template
+*/
+
+app.engine("view", function(path, opt, callback){
+	fs.readFile(path, function(err, content){
+		if(err) return callback(err);
+		var rendered = content.toString().replace(/\#src\#/g, opt.src);
+    	return callback(null, rendered);
+	});
+});
+app.set("views", __dirname + "/views");
+app.set("view engine", "view");
 
 app.use("/uploads", express.static("uploads") );
 
 app.get("/", function(req, res){
-	res.send(index_page);
+	res.sendFile(__dirname + "/index.html");
+
 });
 
 app.post("/upload",function(req, res){
@@ -21,11 +31,8 @@ app.post("/upload",function(req, res){
 	form.keepExtensions = true;
 
 	form.parse(req, function(err, items, files){
-		res.writeHead(200, {"Content-Type": "text/html"});
 		var fileName = path.parse(files["upload-file"].path);
-		console.log("/uploads/" + fileName.base);
-
-		res.end("File uploaded!<br /><video controls src='//localhost:8000" + ("/uploads/" + fileName.base) + "'></video><br /><a href='/'>Back</a>");
+		res.render("upload",{src: ("/uploads/" + fileName.base) });
 	});
 });
 
